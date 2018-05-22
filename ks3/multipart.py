2812159c7b51ecafd04f7fcbbd47382f08730856
wangyaxian <wangyaxian@kingsoft.com>
2018-05-22 15:27:56 +0800

    prevent other bugs

--- ks3/multipart.py
+++ ks3/multipart.py
@@ -39,6 +39,8 @@ class CompleteMultiPartUpload(object):
         return None
 
     def endElement(self, name, value, connection):
+        if name.startswith("ns2"):
+            name = name.split(":")[1]
         if name == 'Location':
             self.location = value
         elif name == 'Bucket':
